fa3c17a579dce90df8b50c15fc454076590f7871
Eddy Kubwimana <92218436+EddyKubwimana@users.noreply.github.com>
2022-12-22 11:39:52 +0000

    Add files via upload
    added insert head, in the middle

--- Linked_list.py
+++ Linked_list.py
@@ -1,12 +1,63 @@
-class Node:
-    def __init__(self, data):
+class Node :
+    def __init__(self,data):
         self.data = data
         self.next = None
-class linkedlist:
+
+
+class llist:
     def __init__(self):
         self.head = None
-    def insert(self,data):
-        new_node = Node(data)
-        if self.head == None:
+
+    def insert(self,node):
+        new_node = Node(node)
+        if self.head is None:
             self.head = new_node
-        else:
+            return
+
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+
+        
+
+    def print_list(self):
+        cur_node = self.head
+        while cur_node:
+            print(cur_node.data)
+            cur_node = cur_node.next
+
+    def insert_head(self,node):
+        new_node = Node(node)
+        new_node.next = self.head
+        self.head = new_node
+    def insert_node(self,after, node):
+        cur_node= self.head
+        while cur_node.next:
+            if cur_node.data == after:
+                new_node = Node(node)
+                new_node.next = cur_node.next
+                cur_node.next = new_node
+            cur_node = cur_node.next
+        return "the node doesn't exist"
+
+    def delete_node(self,node):
+        pass
+                
+           
+        
+            
+
+l = llist()
+l.insert(10)
+l.insert(20)
+l.insert(40)
+l.insert(70)
+l.insert_head(80)
+l.print_list()
+print("new_one")
+l.insert_node(10,300)
+l.insert_node(10,500)
+l.print_list()
+
+    
